899e78c73a79a7d8cc9020b42d4ca8e3efa9d107
Ruqaya99Maed <90410498+Ruqaya99Maed@users.noreply.github.com>
2022-05-02 22:29:21 +0300

    Update dartQ1solution .dart

--- dartQ1solution .dart
+++ dartQ1solution .dart
@@ -1,51 +1,6 @@
-abstract class Animal{
-
-  void printName();
-  void printSound();
+main(){
+  int hour = DateTime.now().hour;
+  print(hour);
 }
-class Cat extends Animal {
-  @override 
-  void printName(){
-    print('The Animal Sound is CAt');
-  }
-  @override
-  void printSound(){
-    print('The Animal Sound is meoo');
-
-  }
-}
-class Dog extends Animal {
-  @override
-  void printName(){
-    print('The Animal Name is Dog');
-
-  }
-  @override
-  void printSound(){
-    print('The Animal Sound is meoo');
-  }
-
-}
-class Cow extends Animal {
-  @override
-  void printName(){
-    print('The Animal Name is Cow');
-  }
-  @override
-  void printSound() {
-    print('The Animal Sound is meoo');
-  }
-}
-void main() {
-  Cat ncat = Cat();
-  ncat.printName();
-  ncat.printSound();
-
-  Dog ndog = Dog();
-  ndog.printName();
-  ndog.printSound();
-
-  Cow ncow = Cow();
-  ncow.printName();
-  ncow.printSound();
-}
+/* The variable is declared with a constant value const and the called function
+is a variable (time function) we must delete the const and we define the variable hour by using var or int */
