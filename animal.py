fa532379d0981179b21d96d6375fab1717f83238
Ruqaya99Maed <90410498+Ruqaya99Maed@users.noreply.github.com>
2022-05-02 23:16:11 +0300

    Update animal.py

--- animal.py
+++ animal.py
@@ -1,1 +1,29 @@
+     
+class Animal():
+    def _init_(self, name, sound):
+        self.name =  name
+        self.sound = sound
+    def printName(self):
+        print (f'My name is {self.name}')
+    def printSound (self): print (f'My sound is {self.sound}')
 
+class Dog(Animal):
+        def _init_(self, name, sound):
+            self.name = name
+            self.sound = sound
+
+class Cat (Animal):
+    def _init_(self, name, sound):
+        self.name = name
+        self.sound = sound
+
+class Cow (Animal):
+    def _init (self, name, sound):
+        self.name = name
+        self.sound = sound
+
+dog1 = Dog("semo", "haw haw")
+
+dog1.printSound()
+
+dog1.printName()   
