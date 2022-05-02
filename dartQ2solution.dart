9630729ed5bd0d8e5c905c0d50d3dd64a0c7ffd6
Ruqaya99Maed <90410498+Ruqaya99Maed@users.noreply.github.com>
2022-05-02 22:15:56 +0300

    Update dartQ2solution.dart

--- dartQ2solution.dart
+++ dartQ2solution.dart
@@ -1,1 +1,51 @@
+abstract class Animal{
 
+  void printName();
+  void printSound();
+}
+class Cat extends Animal {
+  @override 
+  void printName(){
+    print('The Animal Sound is CAt');
+  }
+  @override
+  void printSound(){
+    print('The Animal Sound is meoo');
+
+  }
+}
+class Dog extends Animal {
+  @override
+  void printName(){
+    print('The Animal Name is Dog');
+
+  }
+  @override
+  void printSound(){
+    print('The Animal Sound is meoo');
+  }
+
+}
+class Cow extends Animal {
+  @override
+  void printName(){
+    print('The Animal Name is Cow');
+  }
+  @override
+  void printSound() {
+    print('The Animal Sound is meoo');
+  }
+}
+void main() {
+  Cat ncat = Cat();
+  ncat.printName();
+  ncat.printSound();
+
+  Dog ndog = Dog();
+  ndog.printName();
+  ndog.printSound();
+
+  Cow ncow = Cow();
+  ncow.printName();
+  ncow.printSound();
+}
